2e5de9d2a9111579f4053b1c7baa6b1241a47dcc
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-10 23:43:09 +0300

    feat:add bands query

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,19 @@ import { userSchema } from './modules/users/user.schema.js';
 import { jwtSchema } from './modules/jwt/jwt.schema.js';
 import { genresSchema } from './modules/genres/genres.schema.js';
 import { artistSchema } from './modules/artists/artist.schema.js';
+import { bandSchema } from './modules/bands/bands.schema.js';
 
 import { userResolver } from './modules/users/user.resolver.js';
 import { jwtResolver } from './modules/jwt/jwt.resolver.js';
 import { genreResolver } from './modules/genres/genres.resolver.js';
 import { artistResolver } from './modules/artists/artist.resolver.js';
+import { bandsResolver } from './modules/bands/bands.resolver.js';
 
 import { UsersService } from './modules/users/user.service.js';
 import { GenreService } from './modules/genres/genres.service.js';
 import { JwtService } from './modules/jwt/jwt.service.js';
 import { ArtistService } from './modules/artists/artist.service.js';
+import { BandsService } from './modules/bands/bands.service.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -24,11 +27,12 @@ const typeDefs = gql`
 	${jwtSchema}
 	${genresSchema}
 	${artistSchema}
+	${bandSchema}
 `;
 
 const server = new ApolloServer({
 	typeDefs,
-	resolvers: merge(userResolver, jwtResolver, genreResolver, artistResolver),
+	resolvers: merge(userResolver, jwtResolver, genreResolver, artistResolver, bandsResolver),
 	csrfPrevention: true,
 	cache: 'bounded',
 	context: ({ req }) => {
@@ -41,6 +45,7 @@ const server = new ApolloServer({
 			jwtService: new JwtService(),
 			genreService: new GenreService(),
 			artistService: new ArtistService(),
+			bandsService: new BandsService(),
 		};
 	},
 });
