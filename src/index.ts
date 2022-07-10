b52e7d4cbc77363ac4f41c3f16b53c37aeb40552
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-10 22:56:33 +0300

    refactor:refactor filenames

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import merge from 'lodash.merge';
 
 import { ApolloServer, gql } from 'apollo-server';
 import { userSchema } from './modules/users/user.schema.js';
-import { jwtSchema } from './modules/jwt/jvt.schema.js';
+import { jwtSchema } from './modules/jwt/jwt.schema.js';
 import { genresSchema } from './modules/genres/genres.schema.js';
 
 import { userResolver } from './modules/users/user.resolver.js';
-import { jwtResolver } from './modules/jwt/jvt.resolver.js';
+import { jwtResolver } from './modules/jwt/jwt.resolver.js';
 import { genreResolver } from './modules/genres/genres.resolver.js';
 
 import { UsersService } from './modules/users/user.service.js';
 import { GenreService } from './modules/genres/genres.service.js';
-import { JwtService } from './modules/jwt/jvt.service.js';
+import { JwtService } from './modules/jwt/jwt.service.js';
 
 const PORT = process.env.PORT || 3000;
 
