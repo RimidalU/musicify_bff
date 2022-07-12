6abe9b9e6792e67f209312a7efe6e1a94163d1c4
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-12 23:44:26 +0300

    feat: add tracks query

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { genresSchema } from './modules/genres/genres.schema.js';
 import { artistSchema } from './modules/artists/artist.schema.js';
 import { bandSchema } from './modules/bands/bands.schema.js';
 import { albumsSchema } from './modules/albums/albums.schema.js';
+import { tracksSchema } from './modules/tracks/tracks.schema.js';
 
 import { userResolver } from './modules/users/user.resolver.js';
 import { jwtResolver } from './modules/jwt/jwt.resolver.js';
@@ -15,6 +16,7 @@ import { genreResolver } from './modules/genres/genres.resolver.js';
 import { artistResolver } from './modules/artists/artist.resolver.js';
 import { bandsResolver } from './modules/bands/bands.resolver.js';
 import { albumsResolver } from './modules/albums/albums.resolver.js';
+import { tracksResolver } from './modules/tracks/tracks.resolver.js';
 
 import { UsersService } from './modules/users/user.service.js';
 import { GenreService } from './modules/genres/genres.service.js';
@@ -22,6 +24,7 @@ import { JwtService } from './modules/jwt/jwt.service.js';
 import { ArtistService } from './modules/artists/artist.service.js';
 import { BandsService } from './modules/bands/bands.service.js';
 import { AlbumsService } from './modules/albums/albums.service.js';
+import { TracksService } from './modules/tracks/tracks.service.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -32,6 +35,7 @@ const typeDefs = gql`
 	${artistSchema}
 	${bandSchema}
 	${albumsSchema}
+	${tracksSchema}
 `;
 
 const server = new ApolloServer({
@@ -43,6 +47,7 @@ const server = new ApolloServer({
 		artistResolver,
 		bandsResolver,
 		albumsResolver,
+		tracksResolver,
 	),
 	csrfPrevention: true,
 	cache: 'bounded',
@@ -58,6 +63,7 @@ const server = new ApolloServer({
 			artistService: new ArtistService(),
 			bandsService: new BandsService(),
 			albumsService: new AlbumsService(),
+			tracksService: new TracksService(),
 		};
 	},
 });
---
+++ src/modules/tracks/tracks.service.ts
@@ -0,0 +1,40 @@
+import 'dotenv/config';
+import { ITrack, ITracks } from './ITracks.js';
+import { RESTDataSource } from 'apollo-datasource-rest';
+
+export class TracksService extends RESTDataSource {
+	constructor() {
+		super();
+		this.baseURL = process.env.TRACKS_URL;
+	}
+
+	isAuthorization(request) {
+		if (this.context.token) {
+			request.headers.set('Authorization', this.context.token);
+		}
+	}
+
+	async tracks(limit = 5, offset = 0): Promise<Partial<ITracks>> {
+		const data = await this.get('', { limit: limit, offset: offset });
+		return data.items;
+	}
+	async track(id: string): Promise<Partial<ITrack>> {
+		const data = await this.get(`/${id}`);
+		return data;
+	}
+	async createTrack(data: ITrack): Promise<Partial<ITrack>> {
+		const response = await this.post('', data);
+		return response;
+	}
+	async deleteTrack(id: string): Promise<Partial<ITrack>> {
+		const response = await this.delete('/' + id);
+		return response;
+	}
+	async updateTrack(data: ITrack): Promise<Partial<ITrack>> {
+		const requestData = Object.assign({}, data);
+		const id = data.id;
+		delete requestData.id;
+		const response = await this.put('/' + id, requestData);
+		return response;
+	}
+}
