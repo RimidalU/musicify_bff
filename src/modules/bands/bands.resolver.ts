9b62a710786b72d6192ed50a93177524b5c9fae5
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-14 23:24:04 +0300

    refactor: refactor bands

--- src/modules/bands/bands.resolver.ts
+++ src/modules/bands/bands.resolver.ts
@@ -1,30 +1,35 @@
 export const bandsResolver = {
-  Query: {
-    artist: async (_, { id }, { dataSources }) => {
-      return dataSources.artistsService.artist(id);
-    },
-    artists: async (_, { limit, offset }, { dataSources }) => {
-      return dataSources.artistsService.artists(limit, offset);
-    },
-  },
-  Mutation: {
-    createArtist: async (_, data, { dataSources }) => {
-      return dataSources.artistsService.createArtist(data);
-    },
-    deleteArtist: async (_, { id }, { dataSources }) => {
-      return dataSources.artistsService.deleteArtist(id);
-    },
-    updateArtist: async (_, data, { dataSources }) => {
-      return dataSources.artistsService.updateArtist(data);
-    },
-  },
+	Query: {
+		band: async (_, { id }, { dataSources }) => {
+			return dataSources.bandsService.band(id);
+		},
+		bands: async (_, { limit, offset }, { dataSources }) => {
+			return dataSources.bandsService.bands(limit, offset);
+		},
+	},
+	Mutation: {
+		createBand: async (_, data, { dataSources }) => {
+			return dataSources.bandsService.createBand(data);
+		},
+		deleteBand: async (_, { id }, { dataSources }) => {
+			return dataSources.bandsService.deleteBand(id);
+		},
+		updateBand: async (_, data, { dataSources }) => {
+			return dataSources.bandsService.updateBand(data);
+		},
+	},
 
-  Artist: {
-    bands(parent, _, { dataSources }) {
-      const band = parent.bandsIds.map((id) =>
-        dataSources.bandsService.band(id)
-      );
-      return band;
-    },
-  },
+	Band: {
+		async genres(parent, _, { dataSources }) {
+			const genres = await parent.genresIds.map((id) => dataSources.genreService.genre(id));
+			return genres;
+		},
+	},
+
+	Member: {
+		async artist(parent, _, { dataSources }) {
+			const artist = await dataSources.artistService.artist(parent.artist);
+			return artist;
+		},
+	},
 };
